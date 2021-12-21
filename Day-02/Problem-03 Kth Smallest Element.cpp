d18928549533924d5aa77955522643d5b46ab893
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2021-12-21 18:26:08 +0530

    Create Problem-03 Kth Smallest Element.cpp

---
+++ Day-02/Problem-03 Kth Smallest Element.cpp
@@ -0,0 +1,84 @@
+/*Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+Example 2:
+
+Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4
+Output : 15
+Explanation :
+4th smallest element in the given 
+array is 15.
+Your Task:
+You don't have to read input or print anything. Your task is to complete the function kthSmallest() which takes the array arr[], integers l and r denoting the starting and ending index of the array and an integer K as input and returns the Kth smallest element.
+ 
+ 
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+Constraints:
+1 <= N <= 105
+1 <= arr[i] <= 105
+1 <= K <= N   */
+ 
+
+
+
+
+//Method 1:
+
+//Using sort function solved in O(nlogn) Time.
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        
+        int n=r+1;
+        sort(arr,arr+n);
+        
+        return arr[k-1];
+    }
+};
+
+
+//Method 2:
+
+//Using Max Heap Concept solved in O(nlogk) Time
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        
+        priority_queue <int > p;
+        
+        for(int i=0;i<=r;i++)
+        {
+            p.push(arr[i]);
+            
+            if(p.size()>k)
+                p.pop();
+        }
+        return p.top();
+    }
+};
