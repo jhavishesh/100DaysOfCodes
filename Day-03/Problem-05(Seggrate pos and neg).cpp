f25278ba80129fd4cff9f785660b95bedc8228b8
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2021-12-22 01:55:05 +0530

    Renamed properly

--- Day-03/Problem-05(Seggrate pos and neg).cpp
+++ Day-03/Problem-05(Seggrate pos and neg).cpp
@@ -4,11 +4,14 @@ An array contains both positive and negative numbers in random order. Rearrange 
 
 Examples : Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
 Output: -12 -13 -5 -7 -3 -6 11 6 5
-*/
 
 
-//Naive Approach:
-// create vector and push_back negative elements and after that push_back positive elements.
+
+Naive Approach:
+
+Create vector and push_back negative elements and after that push_back positive elements. */
+
+
 
 void move(int arr[],int n)
 {
