54ec27ec6cc8fc2f232cbe4a483f909115ab23a3
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2021-12-25 23:55:15 +0530

    Union of array.cpp

---
+++ Day-06/Union of Array.cpp
@@ -0,0 +1,20 @@
+
+/* https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1#  */
+
+
+
+
+
+ int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        set <int >v;
+        
+        for(int i=0;i<n;i++)
+            v.insert(a[i]);
+    
+         for(int i=0;i<m;i++)
+            v.insert(b[i]);
+
+     
+     return v.size();
+    }
