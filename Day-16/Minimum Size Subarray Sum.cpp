d71cfd143458ac55b7170dbe7c77b8059241ce59
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-06 00:05:26 +0530

    Minimum Size Subarray Sum

---
+++ Day-16/Minimum Size Subarray Sum.cpp
@@ -0,0 +1,48 @@
+/*  https://leetcode.com/problems/minimum-size-subarray-sum/  */
+
+
+//Naive Solution: O(N^2)
+
+int minSubArrayLen(int s, vector<int>& nums)
+{
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    int ans = INT_MAX;
+    vector<int> sums(n);
+    sums[0] = nums[0];
+    for (int i = 1; i < n; i++)
+        sums[i] = sums[i - 1] + nums[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int sum = sums[j] - sums[i] + nums[i];
+            if (sum >= s) {
+                ans = min(ans, (j - i + 1));
+                break; //Found the smallest subarray with sum>=s starting with index i, hence move to next index
+            }
+        }
+    }
+    return (ans != INT_MAX) ? ans : 0;
+}
+
+
+//OPtimal Solution:
+
+
+int minSubArrayLen(int s, vector<int>& nums)
+{
+    int n = nums.size();
+    int ans = INT_MAX;
+    int left = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += nums[i];
+        while (sum >= s) {
+            ans = min(ans, i + 1 - left);
+            sum -= nums[left++];
+        }
+    }
+    return (ans != INT_MAX) ? ans : 0;
+}
+
+
