9306607c32831a157f9806dca35b34d36509697e
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-08 19:45:04 +0530

    Count ways to N'th Stair

---
+++ Day-18/Count ways to N'th Stair.cpp
@@ -0,0 +1,9 @@
+/* https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/# */
+
+//Solution:
+
+long long countWays(int m)
+    {
+        long long ans=m/2+1;  // when m is even it increases by 1
+        return ans;
+    }
