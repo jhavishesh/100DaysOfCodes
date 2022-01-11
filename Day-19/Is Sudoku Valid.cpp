543920630ff6900a98ab3a1fa90c52171038f4ab
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-11 11:26:34 +0530

    Is Sudoku Valid

---
+++ Day-19/Is Sudoku Valid.cpp
@@ -0,0 +1,22 @@
+/* https://practice.geeksforgeeks.org/problems/is-sudoku-valid4820/1/ */
+
+//Solution:
+
+int isValid(vector<vector<int>> mat){
+        bool row[10][10] = {false}, col[10][10] = {false}, grid[10][10] = {false};
+        
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                
+                int num = mat[i][j];
+                if(num == 0) continue;
+                int grid_idx = (i/3) * 3 + (j/3);
+                
+                if(row[i][num] || col[j][num] || grid[grid_idx][num])
+                    return 0;
+                
+                row[i][num] = col[j][num] = grid[grid_idx][num] = true;
+            }
+        }
+        return 1;
+    }
