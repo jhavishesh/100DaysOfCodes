000eddaa46acec4ba8443fa04ed7f734dc5d7096
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-17 23:46:12 +0530

    Find All Four Sum Numbers

---
+++ Day-27/Find All Four Sum Numbers.cpp
@@ -0,0 +1,41 @@
+/* https://practice.geeksforgeeks.org/problems/find-all-four-sum-numbers1732/1  */
+
+//Solution:
+
+  vector<vector<int> > fourSum(vector<int> &arr, int k) {
+         int n = arr.size();
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> ans;
+        set <vector<int>> s;
+        for (int i=0; i<n; i++)
+        {
+            for (int j=i+1; j<n; j++)
+            {
+                int start = j+1;
+                int end = n-1;
+                while (start < end)
+                {
+                    int sum = arr[i]+arr[j]+arr[start]+arr[end];
+                    if (sum == k)
+                    {
+                        s.insert({arr[i],arr[j],arr[start],arr[end]});
+                        start++;
+                        end--;
+                    }
+                    else if (sum > k)
+                    {
+                        end--;
+                    }
+                    else 
+                    {
+                        start++;
+                    }
+                }
+            }
+        }
+        for(auto i:s)
+        {
+            ans.push_back(i);
+        }
+        return ans;
+    }
