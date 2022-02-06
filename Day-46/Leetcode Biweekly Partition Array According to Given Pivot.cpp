79cf207bfdab10ab8e1d121704aac34235394e47
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-02-06 21:52:16 +0530

    Leetcode Biweekly P2

---
+++ Day-46/Leetcode Biweekly Partition Array According to Given Pivot.cpp
@@ -0,0 +1,29 @@
+
+
+//Solution:
+
+class Solution {
+public:
+vector pivotArray(vector& nums, int pivot) {
+vectorres;
+vectorg;
+int p=0;
+for(int i=0;i<nums.size();i++)
+{
+if(nums[i]<pivot)res.push_back(nums[i]);
+else if(nums[i]==pivot)p++;
+else g.push_back(nums[i]);
+}
+
+   for(int i=0;i<p;i++) res.push_back(pivot);
+
+    
+   for(int i=0;i<g.size();i++) res.push_back(g[i]);
+       
+    
+    
+    return res;
+    
+}
+};
+
