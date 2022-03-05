b779f0640106e2909eb7196ac8c437c1ebf30349
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-05 11:35:23 +0530

    Backtracking-01 Rat in Maze

--- Day-50/Rat in Maze.cpp
+++ Day-50/Rat in Maze.cpp
@@ -1,82 +1,36 @@
 /* https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1 */
 
-//This solution is tested and run only on code editor it will get accepted on gfg version
+
 
 //Solution:
 
-#include<bits/stdc++.h>
-using namespace std;
-
-bool ratInMaze(char maze[10][10],int bt[10][10],int i,int j,int m ,int n)
-{ 
-  
-  if(i==m && j==n) //check whether we reach to destinatination
-  { 
-    bt[i][j]=1;   //set value 1 to the path followed
-    //print the path
-    for(int i=0;i<m;i++)
+void allPath(int row,int col,int n,vector<vector<int>> &m,string ans,vector<string> &res)
     {
-      for(int j=0;j<n;j++)
-        cout<<bt[i][j]<<" ";
-        cout<<endl;
+        if(row<0 || row>=n || col<0 || col>=n || m[row][col]==0 )
+            return ; // To check whether you are in mat[][] or not
+        
+        if(row==n-1 && col==n-1) // check we reach to the last cell or not
+        {
+            res.push_back(ans); //Push the path followed
+            return ;
+        }
+        m[row][col]=0; // Start with first cell
+        
+        allPath(row+1,col,n,m,ans+"D",res);  //Go Down
+        allPath(row,col-1,n,m,ans+"L",res);  //Go Left
+        allPath(row,col+1,n,m,ans+"R",res);  //Go Right
+        allPath(row-1,col,n,m,ans+"U",res);  //Go Up
+        m[row][col]=1; // Mark the path followed is visited
+        
+        return;
     }
-      cout<<endl;
-    return true;
-    ctr++;
-  
-  
-  }
-  
-  //rat should be inside the grid
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        
+        string ans="";      //Empty string
+        vector<string>res;  //To Store the Path
+        allPath(0,0,n,m,ans,res);
+        sort(res.begin(),res.end());
+        return res;
+        
+    }
 
-  if(i>m || j>n)
-  {
-    return false;
-  }
-  
-  if(maze[i][j]=='X')
-  {
-    return false;
-  }
-    //assume the solution exist through current cell
-      
-      bt[i][j]=1;
-
-      bool rightSuccess =ratInMaze(maze,bt,i,j+1,m,n);
-      bool downSuccess =ratInMaze(maze,bt,i+1,j,m,n);
-
-
-      //the path followed to reach destinatination when we come back we set the cell's value to 0 i.e. Backtracking 
-      bt[i][j]=0;
-
-      if(rightSuccess||downSuccess)
-      {
-        return true;
-      }
-      return false;
-    
-
-}
-
-int main()
-{
-    char maze[10][10]= { "0000",
-                          "00X0",
-                          "000X",
-                          "0X00",
-    };
-
-  int bt[10][10]={0};
-  int m=4,n=4;
-
-  bool ans = ratInMaze(maze,bt,0,0,m-1,n-1);
-
-  
-
-  if(ans==false)
-    cout<<"There is no Path"<<endl;
-
-
-
-  return 0;
-}
