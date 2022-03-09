c77df9e1373becdb5998c11dfc0d34f4228358c1
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-09 23:06:21 +0530

    Tyre Problem

---
+++ Day-77/Tyre Problem.cpp
@@ -0,0 +1,62 @@
+//https://www.codechef.com/START29D/problems/TYRE
+
+//Solution:
+
+#include <bits/stdc++.h>
+
+using namespace std;
+#pragma GCC optimize("O1")
+#pragma GCC optimize("O2")
+ #pragma GCC optimize("O3")
+ #pragma GCC optimize("Os")
+ #pragma GCC optimize("Ofast")
+ 
+#pragma GCC target("avx,avx2,fma")
+ 
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #VA_ARGS << "):", dbg_out(VA_ARGS)
+#else
+#define dbg(...)
+#endif
+typedef long double LD;
+typedef long long LL;
+typedef int64_t I64;
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+#define LSB(i) ((i) & -(i))
+ 
+
+const int MAX_N = 1e5 + 5;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++)
+     {
+         int N,M;
+
+         cin>>N>>M;
+
+         int b= N*2;
+         int c= M*4;
+         cout<<b+c<<endl;
+    }
+ }
+
+
