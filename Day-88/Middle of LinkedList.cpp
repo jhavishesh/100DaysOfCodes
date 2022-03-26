1b13f9dc9ebd67f891ee331d7218371565ffd62d
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-26 21:47:56 +0530

    Middle of LinkedList

---
+++ Day-88/Middle of LinkedList.cpp
@@ -0,0 +1,17 @@
+// https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1/
+
+
+//Solution:
+
+int getMiddle(Node *head)
+    {
+        Node *slow=head,*fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+        }
+        return slow->data;
+    }
