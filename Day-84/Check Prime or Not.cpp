4a7a910d44186831bbcf9281d7a787582e20eab2
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-26 17:50:49 +0530

    Prime or Not

---
+++ Day-84/Check Prime or Not.cpp
@@ -0,0 +1,16 @@
+// Solution:
+
+
+bool isPrime(int n)
+{
+	// Corner case
+	if (n <= 1)
+		return false;
+
+	// Check from 2 to square root of n
+	for (int i = 2; i <= sqrt(n); i++)
+		if (n % i == 0)
+			return false;
+
+	return true;
+}
