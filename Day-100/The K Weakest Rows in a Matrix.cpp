33ed64743aa54b51693aec2b72ca5f10a9760965
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-31 11:35:56 +0530

    The K Weakest Rows in a Matrix

---
+++ Day-100/The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+//Solution:
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+         vector<int>ans;
+        vector< pair <int,int> > vect;
+        
+        int n=mat.size(), m=mat[0].size();
+        
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==1) count++;
+            }
+            vect.push_back( make_pair(count,i) );
+        }
+        sort(vect.begin(), vect.end());
+        
+        for(int i=0; i<k; i++){
+            ans.push_back(vect[i].second);
+        }
+        return ans;
+    }
+};
