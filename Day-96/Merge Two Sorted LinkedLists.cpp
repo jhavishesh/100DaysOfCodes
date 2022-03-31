80e1877eea8c136f3fc2eac9a99a234a9dbdc6df
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-31 11:27:25 +0530

    Merge Two Sorted LinkedLists

---
+++ Day-96/Merge Two Sorted LinkedLists.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/ 
+
+
+//Solution:
+
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1 || !list2)
+return list1 ? list1 : list2;
+if (list1->val > list2->val)
+swap(list1, list2);
+list1->next = mergeTwoLists(list1->next, list2);
+return list1;
+    }
+};
